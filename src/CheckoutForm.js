852b24ef513fcca32b5cb23944408ebadba7fd74
Sneha <snhthakare@gmail.com>
2020-08-04 22:38:58 +0530

    Updates checkout form

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -6,13 +6,24 @@ class CheckoutForm extends React.Component {
   handleSubmit = async event => {
     event.preventDefault();
 
-    // handle payment request
+    const { stripe, elements } = this.props;
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    const result = await stripe.createToken(card);
+    if (result.error) {
+      console.log(result.error.message);
+    } else {
+      console.log(result.token);
+    }
   };
 
   render() {
     return (
       <div>
-        <div class="product-info">
+        <div className="product-info">
           <h3 className="product-title">Apple MacBook Pro</h3>
           <h4 className="product-price">$999</h4>
         </div>
